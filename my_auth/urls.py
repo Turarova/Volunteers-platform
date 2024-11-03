99799cec5ad65b6702054f7a48e416109ab0624f
Aigerim <maviboncuaika@gmail.com>
2024-11-03 20:20:42 +0600

    add urls files

---
+++ my_auth/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from . import views
+
+urlpatterns = [
+    path('register/', views.RegisterView.as_view()),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', views.LoginView.as_view()),
+    path('activation/', views.ActivationView.as_view()),
+    path('users/', views.UserListAPIView.as_view()),
+    path('logout/', views.LogoutAPIView.as_view())
+]
